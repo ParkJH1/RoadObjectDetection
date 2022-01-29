0b81c9f00ea95083060f715538128b13c5c4bfeb
박정현 <pjh9996@naver.com>
2022-01-29 17:55:18 +0900

    Update 03. load_json.py

--- lab_data/03. load_json.py
+++ lab_data/03. load_json.py
@@ -5,17 +5,25 @@ label = json.load(f)
 
 print(label)
 print(type(label))
+print(label[11])
 print(len(label))
 
-print(label[0])
 print(type(label[0]))
+print(label[0]['name'])
+print(label[0]['attributes']['scene'])
 
 print(label[0].keys())
+print(label[0]['name'])
 print(label[0]['attributes'])
-print(type(label[0]['attributes']))
-
+print(label[0]['timestamp'])
 print(label[0]['labels'])
-print(type(label[0]['labels']))
 
-for label in label[0]['labels']:
-    print(label.keys())
+for l in label[0]['labels']:
+    print(l)
+
+
+for l in label[0]['labels']:
+    print(l.keys())
+
+print(label[0]['labels'][0]['category'])
+print(label[0]['labels'][0]['box2d'])
