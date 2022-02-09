40ed70b07a1af62c38d8f002a6f6b03d9af5d15a
박정현 <pjh9996@naver.com>
2022-02-09 20:55:35 +0900

    Update 06. crop_image.py

--- lab_data/06. crop_image.py
+++ lab_data/06. crop_image.py
@@ -1,9 +1,9 @@
 import cv2
 
-file_name = '9b975a41-1b150816.jpg'
+file_name = 'ae7f4103-b1fbdc93.jpg'
 image = cv2.imread('../data/images/' + file_name)
 
-crop_image = image[100:300, 100:1000]
+crop_image = image[10:100, 10:200]
 
 cv2.imshow('image', image)
 cv2.imshow('crop image', crop_image)
