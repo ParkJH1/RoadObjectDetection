c6b10684f8f209a9fcccbd682702a0694094bce9
박정현 <pjh9996@naver.com>
2022-02-09 20:55:37 +0900

    Update 08. save_image.py

--- lab_data/08. save_image.py
+++ lab_data/08. save_image.py
@@ -4,9 +4,9 @@ import os
 file_name = 'ae7f4103-b1fbdc93.jpg'
 image = cv2.imread('../data/images/' + file_name)
 
-resize_image = cv2.resize(image, (224, 224))
+resize_image = cv2.resize(image, (100, 200))
 
-if not os.path.exists('resize_image'):
-    os.mkdir('resize_image')
+if not os.path.exists('abcd'):
+    os.mkdir('abcd')
 
-cv2.imwrite('resize_image/0.jpg', resize_image)
+cv2.imwrite('abcd/1.jpg', resize_image)
