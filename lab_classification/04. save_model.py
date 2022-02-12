ac2bb65475552471019b49d02b658a7dd3432c84
박정현 <pjh9996@naver.com>
2022-02-12 16:54:34 +0900

    Create 04. save_model.py

---
+++ lab_classification/04. save_model.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import os
+
+model = tf.keras.applications.MobileNet(
+    input_shape=(224, 224, 3),
+    include_top=False,
+    weights='imagenet'
+)
+
+model.trainable = False
+
+model = tf.keras.Sequential([
+    model,
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(9),
+    tf.keras.layers.Softmax()
+])
+
+model.summary()
+
+if not os.path.exists('../models'):
+    os.mkdir('../models')
+
+model.save('../models/mymodel.h5')
