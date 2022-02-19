96cba052fd021fd600806d091d4c4bb7d2d7f0e9
박정현 <pjh9996@naver.com>
2022-02-19 17:52:41 +0900

    Create model.py

---
+++ final_classification/model.py
@@ -0,0 +1,33 @@
+class Model:
+    def __init__(self):
+        pass
+
+    # 데이터 불러오기
+    def load_data(self):
+        print('load_data')
+        pass
+
+    # 모델 구축
+    def build(self):
+        pass
+
+    # 모델 학습
+    def train(self):
+        pass
+
+    # 예측
+    def predict(self):
+        pass
+
+    # 모델 저장
+    def save(self):
+        pass
+
+    # 모델 불러오기
+    def load(self):
+        pass
+
+
+if __name__ == '__main__':
+    model = Model()
+    print('asdf')
