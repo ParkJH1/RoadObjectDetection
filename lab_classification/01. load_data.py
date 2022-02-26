8977dc818af4d16e46ca8e4c8d1cf535d8707a95
박정현 <pjh9996@naver.com>
2022-02-26 14:41:13 +0900

    Update 01. load_data.py

--- lab_classification/01. load_data.py
+++ lab_classification/01. load_data.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     '../classification_data/',
     image_size=(224, 224),
-    label_mode='categorical',
+    label_mode='categorical'
 )
 
 data = train_dataset.take(1)
@@ -11,3 +11,4 @@ for images, labels in data:
     print(images, labels)
 
 print(train_dataset.class_names)
+# 원핫 인코딩
