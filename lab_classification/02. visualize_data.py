ab336133fd122c965d281c74a36c785fca804700
박정현 <pjh9996@naver.com>
2022-02-26 14:41:15 +0900

    Update 02. visualize_data.py

--- lab_classification/02. visualize_data.py
+++ lab_classification/02. visualize_data.py
@@ -13,11 +13,24 @@ data = train_dataset.take(1)
 plt.figure(0)
 plt.title('data')
 
+image_list = []
+label_list = []
+
 for images, labels in data:
     for i in range(9):
         plt.subplot(3, 3, i + 1)
         plt.imshow(images[i].numpy().astype('uint8'))
         plt.title(train_dataset.class_names[np.argmax(labels[i])])
         plt.axis('off')
+        image_list.append(images[i])
+        label_list.append(labels[i])
+
+plt.figure(1)
+plt.title('second')
+for i in range(9):
+    plt.subplot(3, 3, i + 1)
+    plt.imshow(image_list[i].numpy().astype('uint8'))
+    plt.title('predict')
+    plt.axis('off')
 
 plt.show()
